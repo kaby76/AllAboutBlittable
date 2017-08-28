300c19b3af9e83e6356a7e8fea84516a73683847
Ken Domino <kenneth.domino@domemtech.com>
2017-08-28 18:18:51 -0400

    Rename.

--- DeepCopyGPU/Properties/AssemblyInfo.cs
+++ DeepCopyGPU/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AllAboutBlittable")]
+[assembly: AssemblyTitle("DeepCopyGPU")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AllAboutBlittable")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("DeepCopyGPU")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
